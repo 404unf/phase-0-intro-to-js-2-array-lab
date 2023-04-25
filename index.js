781fc3a23e827dab3535f37bda0ae59509201bef
Anna <34191811+404unf@users.noreply.github.com>
2023-04-24 18:17:54 -0700

    Test driven Arrays

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // Write your solution here!
+
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name) {
+    cats.push(name)
+}
+
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name)
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop()
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift()
+}
+
+function appendCat(name) {
+    const new_cats = [...cats]
+    new_cats.push(name)
+    return new_cats
+}
+
+
+function prependCat(name) {
+    const newCats = [...cats]
+    newCats.unshift(name)
+    return newCats
+}
+
+function removeLastCat() {
+    const copyOfCats = [...cats]
+    copyOfCats.pop()
+    return copyOfCats
+}
+
+
+function removeFirstCat() {
+    const copyOfCats = [...cats]
+    copyOfCats.shift()
+    return copyOfCats
+
+}
